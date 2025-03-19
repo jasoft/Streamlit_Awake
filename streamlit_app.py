6744776598cf0f2497378c4085136ff999c76a24
Shubhwithai <shubham@buildfastwithai.com>
2025-03-19 19:21:18 +0530

    Updated All Files

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+# List of Streamlit App URLs
+
+STREAMLIT_APPS = [
+    "https://educhain-chatbot.streamlit.app/",
+    "https://gemini-multimodal.streamlit.app/",
+    "https://deepseek-r1-groq-chat.streamlit.app/",
+    "https://notebooklm-podcast.streamlit.app/",
+    "https://educhain-visual-question-generator.streamlit.app/",
+    "https://synthetic-data-generation-buildfastwithai.streamlit.app/",
+    "https://image-story-generator-buildfastwithai.streamlit.app/",
+    "https://calorie-advisor-app.streamlit.app/#e51f5cb4",
+    "https://resumoid.streamlit.app/",
+    "https://audiosummarizationapp.streamlit.app/",
+    "https://medical-description-extractor.streamlit.app/",
+    "https://medical-description-extractor.streamlit.app/",
+    "https://flashcardgenerator.streamlit.app/",
+    "https://educhain-youtube-qna.streamlit.app/",
+    "https://educhain-qna-generator.streamlit.app/",
+    "https://educhain-document-qna.streamlit.app/",
+    "https://educhain-multimodal-qna.streamlit.app/",
+    "https://educhain-multimodal-qna.streamlit.app/",
+    "https://educhain-interactive-app.streamlit.app/",
+    "https://multi-lang-quiz.streamlit.app/",
+    "https://deep-research-search.streamlit.app/",
+]
